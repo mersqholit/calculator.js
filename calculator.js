132171028e447ef5ccc7242dc33390bd4041992b
mersqholit <raphealhammersqholit@gmail.com>
2025-02-20 22:35:15 +0100

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,27 @@
+function calculator() {
+    let num1 = parseFloat(prompt("Enter first number:"));
+    let operator = prompt("Enter operator (+, -, *, /):");
+    let num2 = parseFloat(prompt("Enter second number:"));
+
+    let result;
+    switch (operator) {
+        case "+":
+            result = num1 + num2;
+            break;
+        case "-":
+            result = num1 - num2;
+            break;
+        case "*":
+            result = num1 * num2;
+            break;
+        case "/":
+            result = num2 !== 0 ? num1 / num2 : "Error! Division by zero.";
+            break;
+        default:
+            result = "Invalid operator!";
+    }
+
+    alert("Result: " + result);
+}
+
+calculator();
